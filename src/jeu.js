23a9648efe4f31a7b54f1bfb552be7d5da2412d0
AnthonyPierard <57867046+AnthonyPierard@users.noreply.github.com>
2022-02-17 10:47:44 +0100

    début interface web

---
+++ src/jeu.js
@@ -0,0 +1,15 @@
+
+class Equipe {
+    constructor(){
+        this.cartes = [Math.random() *12, Math.random() *12,Math.random() *12,Math.random() *12,Math.random() *12];
+    }
+
+}
+
+function load(){
+    const bel = new Equipe();
+    const it = new Equipe();
+    const hol = new Equipe();
+    const all = new Equipe();
+    
+}
